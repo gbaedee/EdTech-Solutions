a26f85afb3ac0789210e658f7088af1147adc16e
Powell, Anthony <powell.anthony.e@gmail.com>
2017-10-11 11:39:19 -0500

    Change the tint of the background image for the header

--- js/init.js
+++ js/init.js
@@ -1,20 +1,21 @@
 //Hook up the tweet display
 
-$(document).ready(function() {
-						   
-	$("#countdown").countdown({
-				date: "25 december 2014 12:00:00",
-				format: "on"
-			},
-			
-			function() {
-				// callback function
-			});
+$(document).ready(start);	
+var start = function() {
+	
+$("#countdown").countdown({
+date: "25 december 2014 12:00:00",
+format: "on"
+},
 
-    $(".tweet").tweet({
-        username: "flashuser",
-        count: 3,
-        loading_text: "loading tweets..."
-    });
+function() {
+// callback function
+});
 
-});	
+$(".tweet").tweet({
+username: "flashuser",
+count: 3,
+loading_text: "loading tweets..."
+});
+
+}
